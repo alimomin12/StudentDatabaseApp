a3cbead77026e32a810c2a440a9e40cac0c40edb
Ali Momin <DarkLord@Alis-Air.hsd1.tx.comcast.net>
2018-07-25 16:28:08 -0500

    Initial commit

---
+++ src/com/company/StudentDatabaseApp.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class StudentDatabaseApp {
+
+    public static void main (String[] args) {
+
+        // Ask how many students we want to add
+        System.out.println("Enter number of students to enroll: ");
+        Scanner in = new Scanner (System.in);
+        int numOfStudents = in.nextInt();
+        Student[] students = new Student[numOfStudents];
+
+        // Create n number of students
+        for (int n = 0; n < numOfStudents; n++) {
+            students[n] = new Student();
+            students[n].enroll();
+            students[n].payTuition();
+
+        }
+
+        for (int n = 0; n < numOfStudents; n++) {
+            System.out.println(students[n].toString()); 
+        }
+    }
+}
